ba100cbad9f8cacbf73b9192e797d86f3b9a0259
jdarwood007 <unmonitored+github@sleepycode.com>
2019-12-29 14:59:11 -0800

    More headers

--- smfinfo.php
+++ smfinfo.php
@@ -236,11 +236,12 @@ elseif (file_exists('./Themes/default/images/smflogo.png'))
 else
 	$smflogo = './Themes/default/images/smflogo.gif';
 
-echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"', !empty($txt['lang_rtl']) ? ' dir="rtl"' : '', '>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=', isset($txt['lang_character_set']) ? $txt['lang_character_set'] : 'UTF-8', '" />
-		<title>', $txt['title'], '</title>
+echo '<!DOCTYPE html>
+<html', $txt['lang_rtl'] == true ? ' dir="rtl"' : '', '>
+<head>
+	<meta charset="', isset($txt['lang_character_set']) ? $txt['lang_character_set'] : 'UTF-8', '">
+	<meta name="robots" content="noindex">
+	<title>', $txt['title'], '</title>
 
 		<style type="text/css">
 			body
