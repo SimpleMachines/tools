1dd71d62cc432c19e9546b78dee902e1a5ce091d
Jeremy D <unmonitored+github@sleepycode.com>
2022-01-15 10:05:22 -0800

    Update the version for the downloads site.

--- repair_settings.php
+++ repair_settings.php
@@ -5,11 +5,11 @@
  *
  * @package SMF
  * @author Simple Machines
- * @copyright 2020 Simple Machines
+ * @copyright 2022 Simple Machines
  * @license https://www.simplemachines.org/about/smf/license.php BSD
  *
- * @version 2.1.2
- * @updated 2020-08-26
+ * @version 2.1.3
+ * @updated 2022-01-15
  */
 
 // We need the Settings.php info for database stuff.
